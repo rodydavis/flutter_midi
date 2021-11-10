dab843a1c9f2cd376ef528c1a612281e9d20dd47
kNoAPP <alldoneb@gmail.com>
2021-11-10 10:58:36 -0800

    Reset note to C

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -69,7 +69,7 @@ class _MyAppState extends State<MyApp> {
             ElevatedButton(
               child: Text('Play C'),
               onPressed: () {
-                _play(127);
+                _play(60);
               },
             ),
           ],
